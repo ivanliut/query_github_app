21a97298bfc7ef7fbae783d3d603a12a82836283
Ivan Liutko <ivanliutko@gmail.com>
2021-02-13 13:32:40 +0200

    add redux store, redux sagas initial setup

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
+
+import store from './src/redux/store'
+import LoginPage from "./src/components/LoginPage";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>GitHub Query</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <LoginPage />
+          <Text>GitHub Query</Text>
+        </View>
+      </Provider>
   );
 }
 
