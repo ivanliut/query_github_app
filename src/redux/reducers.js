913e7b28c91ef59a9ec19b2beb670b447bd2ccc4
Ivan Liutko <ivanliutko@gmail.com>
2021-02-14 15:41:56 +0200

    add selectors setup

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,9 @@
 import { userReducer } from './user/reducer';
+import { user } from './user/constants';
 
 /**
  * Combine all separate reducers to Root-reducer;
  */
 export default {
-  user: userReducer,
+  [user]: userReducer,
 };
